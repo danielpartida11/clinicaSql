c550a8a311a0c2056625ce0a3dc81c690ef39a58
Pablo Partida Caballero <danielpartida32@hotmail.com>
2020-11-06 10:36:36 +0100

    iniciado un proyecto sequelize

---
+++ app.js
@@ -0,0 +1,30 @@
+//-------------------------------------------REQUIRES-----------------------------------------------------------
+const { static } = require('express');
+const express = require ('express');
+
+const mysql = require ('mysql2/promise');
+//-----------------------------------------REQUIRES-------------------------------------------------------------
+
+//--------------------------------------------PORT--------------------------------------------------------------
+const app = express();
+
+const PORT = 5000;
+//----------------------------------------------------PORT------------------------------------------------------
+
+//--------------------------------------CONEXION CON LA BBDD----------------------------------------------------
+const connect = mysql.createConnection({
+
+    host: 'localhost',
+    user: 'root',
+    database: 'clinicaSql'
+})
+//-----------------------------------------CONEXION CON LA BBDD-------------------------------------------------
+
+app.use(express.json());
+
+app.use(function(req, res, next) { //para evitar el error CORS
+    res.header("Access-Control-Allow-Origin", "*"); //permite hacer peticiones desde todos los orígenes
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept"); //permite peticiones con las cabeceras enumeradas
+    // res.header("Access-Control-Allow-Methods", "GET, POST");
+    next();
+});
