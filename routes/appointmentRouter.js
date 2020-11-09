4d009faa3e2b4026941bd224e79736c858e2a6c4
Pablo Partida Caballero <danielpartida32@hotmail.com>
2020-11-09 22:21:32 +0100

    creado y completado appointment controller

---
+++ routes/appointmentRouter.js
@@ -0,0 +1,17 @@
+//------------------------------------------REQUIRES------------------------------------------------------------
+
+const router = require('express').Router();
+const controller = require('./servicesMovie');
+module.exports = router;
+
+//-------------------------------------------REQUIRES-----------------------------------------------------------
+
+//-------------------------------------------CONTROLLERS--------------------------------------------------------
+router.get('/appointment', controller.getappointment);
+
+router.post('/appointment', controller.createappointment);
+
+router.put('/appointment', controller.updateappointment);
+
+router.delete('/appointment/:id', controller.deleteappointment);
+//-------------------------------------------CONTROLLERS--------------------------------------------------------
