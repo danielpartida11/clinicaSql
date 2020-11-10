5f5b815e94783fb94e05612e2688e163c8d20ffe
Pablo Partida Caballero <danielpartida32@hotmail.com>
2020-11-10 01:12:20 +0100

    creado y completado el clinic router

---
+++ routes/clinicRouter.js
@@ -0,0 +1,17 @@
+//------------------------------------------REQUIRES------------------------------------------------------------
+
+const router = require('express').Router();
+const controller = require('./servicesMovie');
+module.exports = router;
+
+//-------------------------------------------REQUIRES-----------------------------------------------------------
+
+//-------------------------------------------CONTROLLERS--------------------------------------------------------
+router.get('/clinic', controller.getclinic);
+
+router.post('/clinic', controller.createclinic);
+
+router.put('/clinic', controller.updateclinic);
+
+router.delete('/clinic/:id', controller.deleteclinic);
+//-------------------------------------------CONTROLLERS--------------------------------------------------------
