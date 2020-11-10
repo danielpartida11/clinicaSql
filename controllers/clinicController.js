c73c856aecd21970dec75e66ffe14b6d0e9382b7
Pablo Partida Caballero <danielpartida32@hotmail.com>
2020-11-10 09:40:15 +0100

    terminado el controller clinic

---
+++ controllers/clinicController.js
@@ -0,0 +1,40 @@
+//-----------------------------------------GET------------------------------------------------------------------
+module.exports.clinic = async(req, res) =>{
+
+    const clinics = await clinic.find();
+    res.json(clinics);
+
+};
+//-------------------------------------------------------GET----------------------------------------------------
+
+//----------------------------------------------POST------------------------------------------------------------
+module.exports.createclinic = async(req, res) =>{
+
+    const clinic = new Clinic(req.body);
+    await clinic.save();
+    res.json(clinic);
+
+};
+//-----------------------------------------------------POST-----------------------------------------------------
+
+//-----------------------------------------------PATH-----------------------------------------------------------
+module.exports.updateClinic = async (req, res) => {
+    
+    const clinic = await Clinic.findById(req.body.id);
+    clinic.name = req.body.name;
+    await clinic.save();
+    res.json(clinic);
+
+};
+//----------------------------------------------------PATH------------------------------------------------------
+
+//----------------------------------------------DELETE----------------------------------------------------------
+module.exports.deleteClinic = async (req, res) => {
+
+    const {id} = req.params;
+    const clinic = await Clinic.findById(id);
+    await Clinic.deleteOne(client);
+    res.json('Saliste de la ClinicaSql');
+
+};
+//----------------------------------------------------DELETE-----------------------------------------------------
