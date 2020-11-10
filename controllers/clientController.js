734009555420569c6db750f14ee456b962bfc594
Pablo Partida Caballero <danielpartida32@hotmail.com>
2020-11-10 09:30:49 +0100

    terminado el client controller

---
+++ controllers/clientController.js
@@ -0,0 +1,40 @@
+//-----------------------------------------GET------------------------------------------------------------------
+module.exports.client = async(req, res) =>{
+
+    const clients = await client.find();
+    res.json(clients);
+
+};
+//-------------------------------------------------------GET----------------------------------------------------
+
+//----------------------------------------------POST------------------------------------------------------------
+module.exports.createappointment = async(req, res) =>{
+
+    const client = new Client(req.body);
+    await client.save();
+    res.json(client);
+
+};
+//-----------------------------------------------------POST-----------------------------------------------------
+
+//-----------------------------------------------PATH-----------------------------------------------------------
+module.exports.updateClient = async (req, res) => {
+    
+    const client = await Client.findById(req.body.id);
+    appointment.name = req.body.name;
+    await client.save();
+    res.json(client);
+
+};
+//----------------------------------------------------PATH------------------------------------------------------
+
+//----------------------------------------------DELETE----------------------------------------------------------
+module.exports.deleteClient = async (req, res) => {
+
+    const {id} = req.params;
+    const client = await Client.findById(id);
+    await Client.deleteOne(client);
+    res.json('Cliente Borrado');
+
+};
+//----------------------------------------------------DELETE-----------------------------------------------------
